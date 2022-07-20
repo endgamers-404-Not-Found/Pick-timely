e086748f05b9929960ed8a387e671db051b7e83c
sujon-chambugong <sujonchambugong@gmail.com>
2022-07-20 21:24:49 +0600

    style changed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import { Routes } from 'react-router-dom';
 import './App.css';
-import Services from './Pages/Home/Services/Services';
 
 function App() {
   return (
     <div>
       <h1 className='text-3xl text-center font-bold'>push code on your branch, don't merge the git without team meeting</h1>
-      <Services></Services>
+      
       
       <Routes>
         {/* <Route path='' element={}></Route> */}
