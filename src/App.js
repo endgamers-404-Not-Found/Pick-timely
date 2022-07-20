c71feb712a2975be76bc53bcb18644168426eb2f
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2022-07-20 12:51:30 +0600

    navbar added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css'; 
+import Header from './Pages/Home/Banner/Banner';
 
 function App() {
   return (
     <div>
-      <h1 className='text-3xl text-center font-bold'>push code on your branch, don't merge the git without team meeting</h1>
-      
+      {/* <h1 className='text-3xl text-center font-bold'>push code on your branch, don't merge the git without team meeting</h1> */}
+
+      <Header />      
       <Routes>
         {/* <Route path='' element={}></Route> */}
         {/* <Route path='' element={}></Route> */}
