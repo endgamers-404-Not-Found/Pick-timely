0a751d3f5a51f48b22e501ac67ad645412eb0312
Azim Chowdhury <azimchy994@gmail.com>
2022-07-20 11:48:17 +0600

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css'; 
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className='text-3xl text-center font-bold'>push code on your branch, don't merge the git without team meeting</h1>
+      
+      <Routes>
+        {/* <Route path='' element={}></Route> */}
+        {/* <Route path='' element={}></Route> */}
+        {/* <Route path='' element={}></Route> */}
+      </Routes>
     </div>
   );
 }
