52e1904a5740399b7bf1e711e6f740eb35a9005b
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2022-07-20 22:51:59 +0600

    trial push

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 
-import { Routes } from 'react-router-dom';
+// import { Routes } from 'react-router-dom';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import './App.css'; 
@@ -16,7 +16,7 @@ import Home from './Pages/Home/Home';
 function App() {
   return (
     <div>
-      <h1 className='text-3xl text-center font-bold'>push code on your branch, don't merge the git without team meeting</h1>
+      
       <Packages></Packages>
       <MobileApps></MobileApps>
 
