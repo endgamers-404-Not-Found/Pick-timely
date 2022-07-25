aee87e1c1ccfc83c67d03e8f09bdd92abe438673
Azim Chowdhury <azimchy994@gmail.com>
2022-07-25 20:44:10 +0600

    auth system done

--- src/Pages/Home/Services/SingleServices/CalendarServices.js
+++ src/Pages/Home/Services/SingleServices/CalendarServices.js
@@ -1,20 +1,15 @@
 import React from 'react';
 
 const CalendarServices = () => {
-
-    const myStyle = {
-        width: 600,
-    }
-
     return (
         <div>
-            <div class="hero mt-20 p-10">
-                <div class="hero-content flex-col lg:flex-row-reverse">
-                    <img style={myStyle} src="https://i.ibb.co/jH6BHYc/calendar.png" class="w-full rounded-lg shadow-2xl" alt="" />
-                    <div className="w-96 lg:mx-10">
-                    <h1 class="text-5xl font-bold">wo-way calendar sync</h1>
-                    <p class="py-6">Connect your team's calendars to Zoho Bookings. Avoid double-booking and increase efficiency. See your schedule for the day, week, and month on our calendar dashboard.</p>
-                    <button class="btn btn-primary">Get Started</button>
+            <div className="hero mt-20 lg:p-10">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <img className='lg:w-[500px] w-[300px] rounded-lg shadow-2xl'src="https://i.ibb.co/jH6BHYc/calendar.png"  alt="" />
+                    <div className="lg:w-96 lg:mx-10">
+                    <h1 className="text-5xl font-bold">Two-way calendar sync</h1>
+                    <p className="py-6">Connect your team's calendars to Zoho Bookings. Avoid double-booking and increase efficiency. See your schedule for the day, week, and month on our calendar dashboard.</p>
+                    <button className="btn btn-primary">Get Started</button>
                     </div>
                 </div>
             </div>
--- src/Pages/Home/Services/SingleServices/CrmUpdate.js
+++ src/Pages/Home/Services/SingleServices/CrmUpdate.js
@@ -2,20 +2,18 @@ import React from 'react';
 
 const CrmUpdate = () => {
 
-    const myStyle = {
-        width: 600,
-    }
+    
 
 
     return (
         <div>
-            <div class="hero mt-20">
-                <div class="hero-content flex-col lg:flex-row-reverse">
-                    <img style={myStyle} src="https://i.ibb.co/YXbCGn7/crm.jpg" class="w-full rounded-lg shadow-2xl" alt="" />
-                    <div className="w-96 mx-10">
-                        <h1 class="text-5xl font-bold">Update your CRM</h1>
-                        <p class="py-6">Keep track of your appointments and plan for follow-up from your CRM. Use our built-in integration with Picktimely CRM or connect to another CRM with PickTimely Flow.</p>
-                        <button class="btn btn-primary">Get Started</button>
+            <div className="hero mt-20">
+                <div className="hero-content flex-col lg:flex-row-reverse">
+                    <img className='lg:w-[500px]  w-[300px] rounded-lg shadow-2xl' src="https://i.ibb.co/YXbCGn7/crm.jpg"  alt="" />
+                    <div className="lg:w-96 mx-2">
+                        <h1 className="text-5xl font-bold">Update your CRM</h1>
+                        <p className="py-6">Keep track of your appointments and plan for follow-up from your CRM. Use our built-in integration with Picktimely CRM or connect to another CRM with PickTimely Flow.</p>
+                        <button className="btn btn-primary">Get Started</button>
                     </div>
                 </div>
             </div>
