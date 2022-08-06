bef94d784de4c05cefeb86a45371579c03a8e8cc
iqbal-hossain <mdiqbalhossain5170@gmail.com>
2022-08-07 00:35:35 +0600

    some chages added

--- src/Pages/Solutions/OtherSolutions/Sales.js
+++ src/Pages/Solutions/OtherSolutions/Sales.js
@@ -31,29 +31,28 @@ const Sales = () => {
 
     return (
         <div>
-            <div class="hero bg-blue-900 p-20 ">
-                <div class="hero-content text-center ">
+             <div class="hero-content text-center">
                     <div class="w-full">
-                        <h1 class="lg:text-5xl text-3xl font-bold mb-10 text-white">In sales and in scheduling, you've got to be well-connected.</h1>
-                        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 place-items-center gap-5 mx-20'>
+                        <h1 class="lg:text-5xl text-2xl font-bold w-full mx-auto mb-10">A In sales and in scheduling, you've got to be well-connected.</h1>
+                        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 place-items-center'>
                             {
-                                sales.map(sale => <div class="card w-80 bg-base-100 shadow-xl">
+                             sales.map(sales => <div class="card w-80 h-[400px] bg-base-100 shadow-xl relative">
                                 <div class="card-body">
-                                    <img src={sale.img} className='w-40 mx-auto' alt="" />
-                                    <h2 class="text-center text-3xl font-bold">{sale.title}</h2>
-                                    <p>{sale.description}</p>
+                                    <img src={sales.img} className='w-24 mx-auto' alt="" />
+                                    <h2 class="text-center text-3xl font-bold">{sales.title}</h2>
+                                    <p>{sales.description}</p>
                                     <div class="card-actions justify-center">
-                                        <button class="btn btn-primary">see details</button>
+                                        <button class="w-full btn btn-primary border-2 px-32 absolute bottom-0">see details</button>
                                     
                                     </div>
                                 </div>
                             </div>)
                             }
+                            
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
     );
 };
 
