2a5103a7cc12e2f4061c3b0c2c406fd72bcd10c9
Meherab211 <meherab395@gmail.com>
2022-08-24 21:11:56 +0600

    pull from hamid

--- src/Pages/Home/FreeTail.js
+++ src/Pages/Home/FreeTail.js
@@ -8,8 +8,8 @@ const FreeTail = () => {
                 <div className="hero-overlay "></div>
                 <div className="hero-content text-center text-neutral-content">
                     <div className="max-w-md">
-                        <h1 className="mb-5 text-5xl font-bold">Get started with a free 15-day trail</h1>
-                        <p className="mb-5">Download our mobile apps and view all your upcoming, pending, \and completed bookings from your phone. Available for the admin and the whole team.</p>
+                        <h1 className="mb-5 text-5xl font-bold"> Try Pictimely free</h1>
+                        <p className="mb-5">After your 14-day trial of our Teams plan, enjoy the Free version of Pictimely – forever..</p>
                         <Link to='/dashboard'><button  className="btn btn-button">Get Started</button></Link>
                     </div>
                 </div>
